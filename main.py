524b49ee237d294994287359d41f9f87c132f492
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-13 16:21:57 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,14 @@
+# Ikkita ro'yxatni birlashtiring va tartiblang
+ro'yxat1 = [3, 6, 9, 12]
+ro'yxat2 = [1, 4, 7, 10]
+
+# Ro'yxatlarni birlashtiramiz
+birlashtirilgan_ro'yxat = ro'yxat1 + ro'yxat2
+
+# Ro'yxatni tartiblash uchun sort() funksiyasidan foydalanamiz
+birlashtirilgan_ro'yxat.sort()
+
+print(birlashtirilgan_ro'yxat)
+```
+
+Natija: `[1, 3, 4, 6, 7, 9, 10, 12]`
